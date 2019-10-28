9ad698d64e717af2c74ad43ef898d9ead059cd67
Egor Bedov <bedovegor2203@gmail.com>
2019-10-28 15:07:08 +0300

    Tag page

--- QA/QA_main/urls.py
+++ QA/QA_main/urls.py
@@ -5,7 +5,10 @@ urlpatterns = [
     path('',            questions_list, name='questions_list_url'),
     path('login/',      login,          name='login_url'),
     path('signup/',     signup,         name='signup_url'),
-    path('question/<str:slug>/', question_detail, name='question_detail_url'),
+    path('question/<str:slug>/',
+    					question_detail,name='question_detail_url'),
     path('ask/',        ask,            name='ask_url'),
     path('tags/',       tags,           name='tags_url'),
+    path('tag/<str:title>/',
+  						tag_detail,		name='tag_detail_url'),
 ]
--- QA/QA_main/views.py
+++ QA/QA_main/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 from .models import *
 from .utils import *
 
+QUESTIONS_PER_PAGE = 3
 
 def questions_list(request):
-    questions = Question.objects.all()
-    context = paginator(request, questions, 3)
+    search_query = request.GET.get('search', '')
+
+    if search_query:
+        questions = Question.objects.filter(Q(title__icontains=search_query) | Q(body__icontains=search_query))
+    else:
+        questions = Question.objects.all()
+
+    context = paginator(request, questions, QUESTIONS_PER_PAGE)
     return render(request, 'QA_main/index.html', context=context)
 
 
@@ -30,3 +38,13 @@ def ask(request):
 def tags(request):
     tags = Tag.objects.all()
     return render(request, 'QA_main/tags.html', context={'tags': tags})
+
+
+def tag_detail(request, title):
+    t = Tag.objects.get(title__iexact=title)
+
+    questions = t.questions.all()
+    context = paginator(request, questions, QUESTIONS_PER_PAGE)
+
+    context['tag'] = t
+    return render(request, 'QA_main/tag_detail.html', context)
